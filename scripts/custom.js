4347fef9808c9c7bfc4d05ef5e6769be080873db
chandrakanth <chandrakanththadakapally@gmail.com>
2017-05-27 22:33:06 -0500

    Make Search functionality using angular in filter

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,4 +1,14 @@
 var myApp = angular
             .module('myApp',[])
             .controller('myController',function($scope){
+                 var employees = [
+                { name: "Ben", gender: "Male", salary: 55000, city: "London" },
+                { name: "Sara", gender: "Female", salary: 68000, city: "Chennai" },
+                { name: "Mark", gender: "Male", salary: 57000, city: "London" },
+                { name: "Pam", gender: "Female", salary: 53000, city: "Chennai" },
+                { name: "Todd", gender: "Male", salary: 60000, city: "London" },
+            ];
+
+            $scope.employees = employees;
+
             });
