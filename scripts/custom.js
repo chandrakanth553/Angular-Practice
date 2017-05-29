1e3af4af270c2d18081acac40ab13e5912b5ff72
chandrakanth <chandrakanththadakapally@gmail.com>
2017-05-29 13:11:19 -0500

    Created HTTP Servicec

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,6 +1,15 @@
 var app = angular
         .module("myModule", [])
-        .controller("myController", function ($scope) {
-
-            
+        .controller("myController", function ($scope,$http,$log) {
+// Creatinh HTTP Service for angular app. That will contain objects
+            $http({
+                    method:'GET',
+                    url:'http://localhost:50633/webservices.asmx/GetAllEmployees'
+            }).then(function(responce){
+                    $scope.employees = responce.data;
+                    $log.info(responce);
+            },function(reason){
+                    $scope.error = reason.data;
+                    $log.info(reason);
+            });
         });
