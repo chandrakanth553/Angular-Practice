19332870f57eca47dfba9dbedfede7550f52ee2b
chandrakanth <chandrakanththadakapally@gmail.com>
2017-05-29 12:40:34 -0500

    Caeated HTTPservice access by Angular JS

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,6 +1,8 @@
 var app = angular
         .module("myModule", [])
-        .controller("myController", function ($scope) {
+        .controller("myController", function ($scope,$http) {
 
-            
+            $http.get('http://localhost:50633/webservices.asmx//GetAllEmployees').then(function(res){
+                $scope.employees = res.data;
+            });
         });
